65bb4ead51bb78c4dc81c48e19fef0f9124d1aae
Thomas Kerber <thomas@tkerber.org>
2016-07-19 18:51:15 +0100

    Correct build script.

--- build.rs
+++ build.rs
@@ -3,8 +3,7 @@ extern crate pkg_config;
 fn main() {
     use std::env::var;
     let target = var("TARGET").unwrap();
-    let target: Vec<_> = target.split('-').collect();
-    if target.get(2) == Some(&"windows") {
+    if target.contains("windows") {
         println!("cargo:rustc-link-lib=dylib=winscard");
     } else {
         println!("cargo:rustc-link-lib=dylib=pcsclite");
